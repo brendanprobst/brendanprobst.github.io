bb31a96fe1a265b09213813d56582986dde60025
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-11-18 17:33:57 -0500

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,43 @@
 import HeaderLink from "./HeaderLink.astro";
 import { SITE_TITLE } from "../config";
+import {
+	FaBrandsGithub,
+	FaBrandsInstagram,
+	FaBrandsTwitter,
+	FaBrandsMedium,
+} from "solid-icons/fa";
 export default function Header() {
 	return (
-		<header class="header flex-row-vstart-hsb">
+		<header class="header">
 			<div class="links">
 				<img
 					src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/xwxrp05i6o8-406%3A773?alt=media&token=41cd0db5-9801-4784-831a-3268fc83937d"
 					alt="Not Found"
 					class="logo"
 				/>
-				<p class="projects">Projects</p>
-				<p class="about_me">About Me</p>
-				<p class="say_hello">Say Hello!</p>
+				<a href="">
+					<p class="projects">Projects</p>
+				</a>
+				<a href="">
+					<p class="about_me">About Me</p>
+				</a>
+				<a href="">
+					<p class="say_hello">Say Hello!</p>
+				</a>
 			</div>
 			<div class="socials">
-				<div class="rectangle-21" />
-				<div class="rectangle-21" />
-				<div class="rectangle-21" />
-				<div class="rectangle-21" />
+				<a href="">
+					<FaBrandsGithub />
+				</a>
+				<a href="">
+					<FaBrandsInstagram />
+				</a>
+				<a href="">
+					<FaBrandsTwitter />
+				</a>
+				<a href="">
+					<FaBrandsMedium />
+				</a>
 			</div>
 		</header>
 	);
