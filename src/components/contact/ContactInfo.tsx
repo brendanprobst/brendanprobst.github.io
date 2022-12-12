c17070a8b8b0ef71dfa3bf232b9d2f13aab074b1
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-12-12 18:59:19 -0500

    qr code

--- src/components/contact/ContactInfo.tsx
+++ src/components/contact/ContactInfo.tsx
@@ -6,6 +6,7 @@ import { BiLogosTelegram } from "solid-icons/bi";
 import { VsLinkExternal } from "solid-icons/vs";
 import { BiRegularCopy } from "solid-icons/bi";
 import type { JSXElement } from "solid-js";
+import QRCode from "qrcode";
 export default function ContactInfo({}) {
 	type Contact = {
 		icon: JSXElement | null;
@@ -38,8 +39,32 @@ export default function ContactInfo({}) {
 	function handleCopy(value: string) {
 		navigator.clipboard.writeText(value);
 	}
-	function handleQRCode(value: string, type: string) {}
-	function handleOpen(value: string, type: string) {}
+	function handleQRCode(value: string, type: string) {
+		QRCode.toDataURL("I am a pony!")
+			.then((url) => {
+				console.log(url);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+
+		// With async/await
+	}
+	const generateQR = async (text) => {
+		try {
+			console.log(await QRCode.toDataURL(text));
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	function handleLink(value: string, type: string) {
+		let result = "";
+		if (type === "mail") {
+			result = "mailto:" + value;
+		}
+		console.log(result);
+		return result;
+	}
 	return (
 		<section id="contact-info">
 			<h2>My Contact Info</h2>
@@ -52,12 +77,15 @@ export default function ContactInfo({}) {
 								<p>{item.value}</p>
 							</div>
 							<div class="icons">
-								<button
-									onClick={() => handleOpen(item.value, item.type)}
+								<a
+									class="button"
+									href={handleLink(item.value, item.type)}
+									target="_blank"
+									rel="noopener"
 									type="button">
 									<VsLinkExternal class="icon" />
 									<span class="hidden">open in app</span>
-								</button>
+								</a>
 								<button onclick={() => handleCopy(item.value)} type="button">
 									<BiRegularCopy class="icon" />
 									<span class="hidden">copy value</span>
